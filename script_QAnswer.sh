7ac7b5a09e15f5fbceaa6933335944201f8aaaf6
dhayananth <dhayanthdharma@gmail.com>
2021-08-13 13:35:48 +0200

    update qanswer sync script

--- script_QAnswer.sh
+++ script_QAnswer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 apt-get install jq
 #import enviroment variables
-source qanswer_secretes.env 
+source qanswer_secretes.env
 export user password
-
 #dump the wikibase content
 cd extensions/Wikibase/
 php repo/maintenance/dumpRdf.php --format "nt" --flavor "full-dump" | grep "http://schema.org/about\|http://schema.org/inLanguage\|http://schema.org/isPartOf" > wikipedia_links.nt
@@ -11,14 +10,14 @@ php repo/maintenance/dumpRdf.php --format "nt" --flavor "truthy-dump" > dump_tru
 cat wikipedia_links.nt dump_truthy.nt > dump.nt
 #login to QAnswer
 echo "Retriving the key ..."
-request=$(curl -X POST 'http://qanswer-core1.univ-st-etienne.fr/api/user/signin' -H 'Content-Type:      application/json' -d '{"usernameOrEmail": "'$user'", "password":"'$password'"}')
+request=$(curl -X POST 'http://qanswer-core1.univ-st-etienne.fr/api/user/signin' -H 'Content-Type: application/json' -d '{"usernameOrEmail": "'$user'", "password":"'$password'"}')
 echo $request
 token=$(echo $request | jq -r '.accessToken')
 echo $token
 #upload and index the dataset to QAnswer
 #echo "Uploading ..."
-#curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/upload?dataset=disabilitywiki' -H "Authorization: Bearer $token" -F file=@dump.nt
+#curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/upload?dataset=test' -H "Authorization: Bearer $token" -F file=@dump.nt
 echo "Updating"
-curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/edit?dataset=disabilitywiki' -H "Authorization: Bearer $token" -F file=@dump.nt
+curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/edit?dataset=test' -H "Authorization: Bearer $token" -F file=@dump.nt
 echo "Indexing ..."
-curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/index' -F 'dataset=disabilitywiki' -F 'properties=http://purl.org/dc/elements/1.1/title,https://schema.org/name,http://www.w3.org/2004/02/skos/core#prefLabel,http://www.w3.org/2004/02/skos/core#altLabel,http://www.w3.org/2000/01/rdf-schema#label,http://www.wikidata.org/prop/direct/P1549,http://purl.org/dc/terms/identifier,http://purl.org/dc/terms/title,https://linkedopendata.eu/prop/direct/P150' -F 'type=https://linkedopendata.eu/prop/direct/P35' -F 'subclass=https://linkedopendata.eu/prop/direct/P302' -H "Authorization: Bearer $token"
+curl -X POST 'https://qanswer-core1.univ-st-etienne.fr/api/dataset/index' --data '{"dataset": "test", "properties": ["http://purl.org/dc/elements/1.1/title","https://schema.org/name","http://www.w3.org/2004/02/skos/core#prefLabel","http://www.w3.org/2004/02/skos/core#altLabel","http://www.w3.org/2000/01/rdf-schema#label","http://www.wikidata.org/prop/direct/P1549","http://purl.org/dc/terms/identifier","http://purl.org/dc/terms/title","https://linkedopendata.eu/prop/direct/P150"], "type": ["https://linkedopendata.eu/prop/direct/P35","https://linkedopendata.eu/prop/direct/P1845"], "subclass": ["https://linkedopendata.eu/prop/direct/P302","https://linkedopendata.eu/prop/direct/P1845"]}' -H 'Content-Type: application/json' -H "Authorization: Bearer $token"
